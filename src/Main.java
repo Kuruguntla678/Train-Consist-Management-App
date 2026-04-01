f9b84c65a96fb73f575df0a8a698cac9bc31de36
Kuruguntla678 <kuruguntlacharankarthikreddy@gmail.com>
2026-04-01 09:25:11 +0530

    UC5: Count Total Bogies

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.LinkedList;
+import java.util.LinkedHashSet;
 
 public class TrainApp {
 
@@ -6,26 +6,16 @@ public class TrainApp {
 
         System.out.println("=== Train Consist Management App ===");
 
-        LinkedList<String> trainConsist = new LinkedList<>();
+        LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
 
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
 
-        System.out.println("Initial Train Consist:");
-        System.out.println(trainConsist);
+        trainFormation.add("Sleeper");
 
-        trainConsist.add(2, "Pantry Car");
-
-        System.out.println("After adding Pantry Car at position 3:");
-        System.out.println(trainConsist);
-
-        trainConsist.removeFirst();
-        trainConsist.removeLast();
-
-        System.out.println("After removing first and last bogie:");
-        System.out.println(trainConsist);
+        System.out.println("Final Train Formation:");
+        System.out.println(trainFormation);
     }
 }
